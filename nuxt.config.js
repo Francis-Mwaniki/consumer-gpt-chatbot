a36e26248b977bdf45c195d4fb10df090984d580
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-26 15:32:15 +0300

    consumer chatgpt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 
 
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss','nuxt-icon','@nuxt/image-edge','@nuxtjs/color-mode','@nuxtjs/supabase'],
+  modules: ['@nuxtjs/tailwindcss','nuxt-icon','@nuxt/image-edge','@nuxtjs/color-mode'],
   buildModules: [
 
   ],
